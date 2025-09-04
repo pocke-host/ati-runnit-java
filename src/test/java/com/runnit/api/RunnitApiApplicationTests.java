fa1409685176391224e6ef1804dfacdec11b6416
pocke-host <quinnhuynh1126@gmail.com>
2025-09-03 20:18:13 -0700

    first setup

---
+++ src/test/java/com/runnit/api/RunnitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runnit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
