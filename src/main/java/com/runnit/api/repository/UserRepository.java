2dbeb73a26caf8d52d3ad1b9a270f26421862149
pocke-host <quinnhuynh1126@gmail.com>
2026-09-09 14:53:17 -0700

    feat: notify mentions and expose race history

--- src/main/java/com/runnit/api/repository/UserRepository.java
+++ src/main/java/com/runnit/api/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUserIgnoreCase(String user);
     Optional<User> findByAuthProviderAndProviderId(User.AuthProvider provider, String providerId);
     boolean existsByEmail(String email);
 
